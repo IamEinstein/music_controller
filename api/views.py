3d18fc4e0bc79598fa729e0bd77706a2ee01f436
Rishit <vermarishit@gmail.com>
2020-12-08 16:06:14 +0530

    Finished 'Handling POST Requests'

--- api/views.py
+++ api/views.py
@@ -1,9 +1,8 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from rest_framework import generics
+from rest_framework import generics, status
 from .models import Room
-from .serializers import RoomSerializer
-import requests
+from .serializers import RoomSerializer, CreateRoomSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
 # Create your views here.
 # * The generics.CreateAPIView  adds a form to create a new model
 # * The generics.ListAPIView only shows the list of current models
@@ -14,7 +13,27 @@ class RoomView(generics.ListAPIView):
     serializer_class = RoomSerializer
 
 
-def get(request):
+class CreateRoomView(APIView):
+    serializer_class = CreateRoomSerializer
 
-    info = requests.get('/api/room')
-    return HttpResponse(info)
+    def post(self, request, format=None):
+        # * 👇 if the checking if there's a session and if it's not, then creating one
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create()
+
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            guest_can_pause = serializer.data.get('guest_can_pause')
+            votes_to_skip = serializer.data.get('votes_to_skip')
+            host = self.request.session.session_key
+            queryset = Room.objects.filter(host=host)
+            if queryset.exists():
+                room = queryset[0]
+                room.guest_can_pause = guest_can_pause
+                room.votes_to_skip = votes_to_skip
+                room.save(update_fields=['guest_can_pause', 'votes_to_skip'])
+            else:
+                room = Room(host=host, guest_can_pause=guest_can_pause,
+                            votes_to_skip=votes_to_skip)
+                room.save()
+        return Response(RoomSerializer(room).data, status=status.HTTP_200_OK)
